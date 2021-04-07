213216d0b49db44176b9eade4a38db1221db1c3c
vivg123 <www.gusain.vivek@gmail.com>
2021-04-07 22:24:50 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -24,9 +24,42 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/timestamp/", (req ,res) => {
+  res.json({unix : Date.now() , utc : Date()}) ;
+})
+
+
+app.get("/api/timestamp/:date" , function (req ,res)
+{
+  const d = req.params.date ;
+  
+  if(/\d{5,}/.test(d))
+  {
+    const dateInt = parseInt(d) ;
+    res.json({unix : dateInt , utc : new Date(dateInt).toUTCString()})
+  }
+
+  else
+  {
+      let dateObject  = new Date(d) ;
+
+      if(dateObject.toString() === "Invalid Date")
+      {
+        res.json({error : "Invalid Date"}) ;
+      }
+
+      else
+      {
+          res.json({unix : dateObject.valueOf() , utc : dateObject.toUTCString()} ) ;
+      }
+  }
+
+})
+
 
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
+  console.log("http://localhost:38515") ;
 });
